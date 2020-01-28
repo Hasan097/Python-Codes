ded92ac7011a5c03fa1464c71a8766b7d1d436ce
Taha Hasan Syed <hpacific110@gmail.com>
2020-01-27 22:09:31 -0800

    Updated Travis

--- Travis.py
+++ Travis.py
@@ -12,11 +12,26 @@ while True:
                   known_users.remove(name)
             elif remove == "n":
                   print("No problem I didnt want you to leave anyway!")
+            else:
+                  print("Invalid input, quitting program by default")
+                  break
       else:
             print("I have'nt met you yet {}".format(name))
             addem = input("Would you like to be added in the list(y/n)? ").strip().lower()
             if addem == "y":
                   known_users.append(name)
+                  print("Success! Welcome to the crew!")
             elif addem == "n":
                   print("No problem I didnt want you anyway!")
-            
+            else:
+                  print("Invalid input, quitting program by default")
+                  break
+      prexit = input("Do you want to continue?(y/n) ").capitalize()
+      if prexit == "Y":
+            print("Splendid lets try this again.")
+            continue
+      elif prexit == "N":
+            break
+      else:
+            print("Invalid input, quitting program by default")
+            break
